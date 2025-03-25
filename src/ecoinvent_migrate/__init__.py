9338c44e5c247582a2815beb244733b0fccfec06
Chris Mutel <cmutel@gmail.com>
2025-03-25 11:17:52 +0100

    0.6.1

--- src/ecoinvent_migrate/__init__.py
+++ src/ecoinvent_migrate/__init__.py
@@ -6,6 +6,6 @@ __all__ = (
     "generate_biosphere_mapping",
 )
 
-__version__ = "0.6"
+__version__ = "0.6.1"
 
 from ecoinvent_migrate.main import generate_biosphere_mapping, generate_technosphere_mapping
